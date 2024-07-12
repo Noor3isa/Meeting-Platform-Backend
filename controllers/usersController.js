ba9a44d48ed7563947584d489084476f81a670f6
noor3isa <noor.mohamed.eisa@gmail.com>
2024-07-13 01:57:29 +0300

    Sending back reports implemented

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -48,7 +48,7 @@ const getReports = async (req, res) => {
             return res.status(404).json({ error: 'No records found for this room' });
         }
 
-        res.json(room, records);
+        res.status(200).json({room, records});
     }
 } catch(err) {
     res.status(500).json({'error': err.message});
